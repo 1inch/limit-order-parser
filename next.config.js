7c8ba56213a24161f8229d028693c7f82049c4b9
limitofzero <limitofzero@gmail.com>
2024-01-20 00:28:18 +0400

    test(): github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/limit-order-parser',
-  assetPrefix: '/limit-order-parser/',
+  output: 'export',
 }
 
 module.exports = nextConfig
