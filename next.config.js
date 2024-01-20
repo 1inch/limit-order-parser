e8c278151a4b6ba1efdb8d3bb84b3e6842becb13
limitofzero <limitofzero@gmail.com>
2024-01-21 00:55:10 +0400

    chore(): update lib and configure prod env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
-  basePath: '/limit-order-parser',
+  basePath: isProd ? '/limit-order-parser' : undefined,
 }
 
 module.exports = nextConfig
